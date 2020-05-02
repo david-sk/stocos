43e2ba91c9b9447abb87fdf1f209aea131e366ce
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-05-02 18:06:26 +0200

    apply clang-format

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,11 +1,11 @@
 ## > clang-format ##################################################################
-# FIND_PROGRAM(CLANGFORMAT_EXECUTABLE clangformat)
-# if (NOT CLANGFORMAT_EXECUTABLE)
-#     packageManager("clang-format")
-#     FIND_PROGRAM(CLANGFORMAT_EXECUTABLE clangformat)
-# endif(NOT CLANGFORMAT_EXECUTABLE)
+FIND_PROGRAM(CLANGFORMAT_EXECUTABLE clang-format)
+if (NOT CLANGFORMAT_EXECUTABLE)
+    packageManager("clang-format")
+    FIND_PROGRAM(CLANGFORMAT_EXECUTABLE clang-format)
+endif(NOT CLANGFORMAT_EXECUTABLE)
 
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h *.hpp)
 foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
     string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
     if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
@@ -17,7 +17,7 @@ add_custom_target(
         clangformat
         COMMAND ${CLANGFORMAT_EXECUTABLE}
         -style=file
-        -i
+        -i 
         ${ALL_SOURCE_FILES}
 )
 ## < clang-format
